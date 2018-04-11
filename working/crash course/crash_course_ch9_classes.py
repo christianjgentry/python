8e32f6035cdb43e52b8139aa16f3bcd1b97769cb
christianjgentry <32580981+christianjgentry@users.noreply.github.com>
2018-04-10 21:04:28 -0500

    1

--- working/crash course/crash_course_ch9_classes.py
+++ working/crash course/crash_course_ch9_classes.py
@@ -24,12 +24,26 @@ class Restaurant():
 			self.number_served = self.number_served + customers
 		else:
 			print("You cannot enter a negative number of customers!")
+			
+class IceCreamStand(Restaurant):
+	#Ice Cream Stand class based of off restaurant class.
+	
+	def __init__(self, name, cuisine="ice cream"):
+	#Takes the attributes from the parent class.
+		super().__init__(name, cuisine)
+		self.flavors = "vanilla, chocolate, strawberry"
+	
+	def describe_flavors(self):
+		#lists the flavors available.
+		print("The flavors that we have available are", self.flavors)
+		
+
 
 foodplace1 = Restaurant("Taco Bell", "Tex-Mex")
 foodplace2 = Restaurant("Olive Garden", "Italian")
 foodplace3 = Restaurant("Texas Road House", "Texan")
-
 restaurant = Restaurant("christian's grill", "bbq")
+mr_freeze = IceCreamStand("mr. freeze")
 
 print(restaurant.number_served)
 restaurant.number_served = 74
@@ -37,12 +51,15 @@ print(restaurant.number_served)
 
 restaurant.increment_number_served(39)
 print(restaurant.number_served)
+
+mr_freeze.describe_flavors()
 '''
 ########################################################################
-
+'''
 class User():
 	def __init__(self, first_name, last_name):
 		#creates a basic instance of a user.
+		self.user_type = "user"
 		self.first_name = first_name
 		self.last_name = last_name
 		self.login_attempts = 0
@@ -51,10 +68,14 @@ class User():
 		#describes the modeled user instance.
 		print("first name:", self.first_name.title())
 		print("last name:", self.last_name.title())
+		print("Your privilege level is set at:", self.user_type)
 	
 	def greet_user(self):
 		#greets the user.
-		print("Greetings,", self.first_name.title() + "!")
+		if self.user_type == "admin":
+			print("Greetings,", self.user_type.title() + "!")
+		else:
+			print("Greetings,", self.first_name.title() + "!")
 		
 	def increment_login_attempts(self):
 		self.login_attempts += 1
@@ -68,9 +89,60 @@ class User():
 			"Login Attempts:", self.login_attempts)
 		
 		
+class Admin(User):
+	#Admin is a child class of User.
+	
+	def __init__(self, first_name, last_name):
+		#initializes from parent class
+		
+		super().__init__(first_name, last_name)
+		self.user_type = "admin"
+		self.privileges = Privileges()
+			
+				
+class Privileges():
+	#Standalone class that models user privileges.
+	
+	def __init__(self, privileges=[]):
+		#Initializes the Privileges class.
+		self.privileges = privileges
+		
+	def add_privilege(self, privilege):
+		#adds privileges to instance.
+		self.privileges.append(privilege)
+		
+	def remove_privilege(self, privilege=""):
+		#removes privilege from instance.
+		print("Current privileges:\n", self.privileges)
+		privilege = input("Type the privilege to remove.")
+		self.privileges.remove(privilege)
+		print("Updated privileges:\n", self.privileges)
+	
+	def show_privileges(self):
+		#lists the privileges available to an admin user.
+		print("As an admin, these are your abilities:\n" 
+			+ str(self.privileges))
+		
+
+admin = Admin("christian", "gentry")
+
+admin.describe_user()
+admin.greet_user()
+admin.privileges.privileges = ["ban user", "add user", "remove post"]
+admin.privileges.show_privileges()
+admin.privileges.remove_privilege()
+'''
+
+
+
+'''	
 user1 = User("christian", "gentry")
 user2 = User("zachery", "gentry")
 user3 = User("chantel", "garcia")
+
+admin.greet_user()
+
+
 
 user1.greet_user()
 user1.describe_user()
@@ -85,8 +157,7 @@ user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.reset_login_attempts()
-
-
+'''
 ########################################################################
 '''
 class Character():
@@ -130,7 +201,7 @@ def brawl(character_1, character_2):
 
 
 christian = Character("christian", atk=16)
-zachery = Character("zachery", hp=150)
+zachery = Character("zachery", hp=100)
 
 print(zachery.hp_current)
 christian.atk_basic(zachery)
