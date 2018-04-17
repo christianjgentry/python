dd07539dd1b09803602ac610cd041d47555fd3a1
christianjgentry <32580981+christianjgentry@users.noreply.github.com>
2018-04-17 00:52:23 -0500

    journals

--- work/revit/journals/tests/revit_journal_read_test_1.py
+++ work/revit/journals/tests/revit_journal_read_test_1.py
@@ -1,5 +1,18 @@
+import dateutil.parser as dparser
+
 #Revit Journal Read Test
-filename = 'journal.0032.worker1.log'
+filename = 'journal.0033.txt'
+'''
+journal = []
+with open(filename, 'r') as f:
+	for line in enumerate(f.readlines()):
+		print(line)
+
+'''
 
 with open(filename, 'r') as f:
-	print(f.read())
+	journal = f.readlines()
+	
+	for line in journal:
+		if "Apr" and "Exit" in line:
+			print(line)
