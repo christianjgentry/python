6ba5431f85bc198894c07637093483db8f9098db
christianjgentry <32580981+christianjgentry@users.noreply.github.com>
2018-04-18 00:08:57 -0500

    1

--- work/revit/journals/modules/journal_parser.py
+++ work/revit/journals/modules/journal_parser.py
@@ -7,6 +7,7 @@ module commonly imported as jparse
 
 #imports
 import json
+import os
 
 
 #classes
@@ -18,7 +19,7 @@ class User():
 		   gathered after initialization'''
 		self.username = username
 		self.title = ""
-		self.journal_log = []
+		self.journal_log = {}
 	
 	def get_dict(self):
 		self.dict = {	
@@ -27,6 +28,21 @@ class User():
 		"journal_log" : self.journal_log,
 					}
 		return self.dict
+	
+	def append_journal_files(self, directory_in_str):
+		directory = os.fsencode(directory_in_str)
+
+		for file in os.listdir(directory):
+			filename = os.fsdecode(file)
+			if filename.endswith(".txt"):
+				with open(filename, 'r') as file_object:
+					if filename not in self.journal_log.keys():
+						self.journal_log[filename] = file_object.readlines()
+					else:
+						continue
+				continue
+			else:
+				continue
 
 
 #functions
@@ -38,4 +54,24 @@ def read_journal_enumerated(filename_journal):
 			enumerated_journal += str(line) + "\n"
 	return enumerated_journal			
 
-print(read_journal_enumerated('journal.0012.txt'))
+def cycle_journal_files(directory_in_str):
+	directory = os.fsencode(directory_in_str)
+
+	for file in os.listdir(directory):
+		filename = os.fsdecode(file)
+		if filename.endswith(".txt"): 
+			print(filename)
+			continue
+		else:
+			continue
+
+
+christian = User("christian")
+
+location = 'C:\\Users\Christian Gentry\AppData\Local\Autodesk\Revit\Autodesk Revit 2018\Journals'			       
+
+christian.append_journal_files(location)
+
+#cycle_journal_files(location)
+
+
