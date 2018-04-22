ba4f6c3508e75d30fd935b33347cc8422dd3853e
Christian Gentry <christian.gentry@mbp-christiangentry.attlocal.net>
2018-04-22 17:19:00 -0500

    Combined Utilities is up and running! SAVE STATE

---
+++ work/revit/journals/modules/isolated parser/Individual Scripts/ram_info.py
@@ -0,0 +1,82 @@
+
+
+
+
+
+#Revit Journal Read Test
+filename = 'journal.0014.txt'
+
+def extract_info_ram(filename):
+	#parse a journal for ram information.
+	
+	#required modules
+	import math
+	
+	#Variables to store journal info to.
+	journal = ""
+	ram_record = []
+	
+	#transform journal into parsable string
+	with open(filename, 'r') as file_object:
+		lines = file_object.readlines()
+		for line in lines:
+			journal = journal + line		
+	journal = journal.splitlines()
+	
+	#Extract the lines that reference ram in the journal.
+	for line in journal:
+		if "ram statistics" in line.lower():
+			data = line.split(':<')[1]
+			ram_record.append([int(s) for s in data.split() if s.isdigit()][0])
+			ram_max = [int(s) for s in data.split() if s.isdigit()][1]
+	
+	#Convert the extracted ram info into usable data.
+	ram_max = math.floor(int(ram_max) / 1000)
+	ram_avg = math.ceil(sum(ram_record) / len(ram_record))
+	ram_peak = sorted(ram_record)[-1]
+	
+	return ram_max, ram_avg, ram_peak
+
+print(extract_info_ram(filename))
+
+
+'''
+
+import matplotlib.pyplot as plt
+
+
+max_ram = 32751
+
+def ram_percentage(dataset):
+	percentage_list = []
+	for value in dataset:
+		percentage = value / max_ram
+		percentage *= 100
+		percentage = math.ceil(percentage)
+		percentage_list.append(percentage)
+	return(percentage_list)
+
+ram_record = []
+with open(filename, 'r') as f:
+	for line in f:
+		line = line.lower()
+		if 'ram statistics' in line:
+			data = line.split(':<')[1]
+			ram_record.append([int(s) for s in data.split() if s.isdigit()][0])
+ram_percentages = ram_percentage(ram_record)
+print(ram_percentages)
+print(ram_record)
+
+
+plt.plot(range(1, 26, 1), ram_percentages, c='red', linewidth = 5)
+
+plt.title("RAM Usage Throughout Revit Session", fontsize=24)
+plt.xlabel("Datapoint", fontsize=14)
+plt.ylabel("RAM Percentage Used", fontsize=14)
+
+#Set size of tick labels.
+plt.tick_params(axis='both', labelsize=14)
+plt.xticks(range(1,26))
+
+plt.show()
+'''
--- work/revit/journals/modules/isolated parser/combined_utilities.py
+++ work/revit/journals/modules/isolated parser/combined_utilities.py
@@ -1,9 +1,9 @@
-import os
-import re
 
 #definitions
 def cycle_journal_files(directory_in_str):
 	#appends all journal files in folder to list journal_files.
+	import os
+	
 	journal_files = []
 	directory = os.fsencode(directory_in_str)
 	for file in os.listdir(directory):
@@ -18,6 +18,8 @@ def cycle_journal_files(directory_in_str):
 
 def extract_info_processor(filename):
 	#parses journal for processor information.
+	import re
+	
 	processor_info = ""
 	with open(filename, 'r') as file_object:
 		lines = file_object.readlines()
@@ -48,6 +50,8 @@ def extract_info_processor(filename):
 
 def extract_info_os(filename):
 	#parses journal for os information.
+	import re
+	
 	os_info = ""
 	with open(filename, 'r') as file_object:
 		lines = file_object.readlines()
@@ -70,57 +74,111 @@ def extract_info_os(filename):
 
 def extract_info_graphics(filename):
 	#parses journal for graphics hardware info.
+	import re
+	
 	graphics_hardware = {}
-	with open(filename, 'r') as file_object:
-		file_object = file_object.read()
-		file_object = file_object.lower().splitlines()
-		for line in file_object:
-			if "video card environment" in line:
-				values = re.findall('"([^"]*)"', line)
-				graphics_hardware["graphics card:"] = values[0]
-				graphics_hardware["manufacturer id:"] = values[1]
-				graphics_hardware["device id:"] = values[2]
-		return graphics_hardware
-
+	
+	try:
+		with open(filename, 'r') as file_object:
+			file_object = file_object.read()
+			file_object = file_object.lower().splitlines()
+			for line in file_object:
+				if "video card environment" in line:
+					values = re.findall('"([^"]*)"', line)
+					graphics_hardware["graphics card:"] = values[0]
+					graphics_hardware["manufacturer id:"] = values[1]
+					graphics_hardware["device id:"] = values[2]
+			return graphics_hardware
+			
+	except:
+		print("***Could not gather GPU info.***")
 
 def extract_info_revit(filename):
 	#parses journal for Revit info.
+	import re
+	
 	revit_info = ""
-	with open(filename, 'r') as file_object:
-		lines = file_object.readlines()
-
-	for i, line in enumerate(lines):
-		if re.search(r"build", line.lower()):
-			for item in lines[max(i-0, 0):i+2]:
-				revit_info = revit_info + item
-
-	#get revit_build
-	revit_build = revit_info.splitlines()[0]
-	revit_build = revit_build.split(":",1)[1].strip()
 	
-	#get revit_build
-	revit_branch = revit_info.splitlines()[1]
-	revit_branch = revit_branch.split(":",1)[1].strip()
-	
-	#return values
-	return revit_build, revit_branch
+	try:
+		with open(filename, 'r') as file_object:
+			lines = file_object.readlines()
+
+		for i, line in enumerate(lines):
+			if re.search(r"build", line.lower()):
+				for item in lines[max(i-0, 0):i+2]:
+					revit_info = revit_info + item
+
+		#get revit_build
+		revit_build = revit_info.splitlines()[0]
+		revit_build = revit_build.split(":",1)[1].strip()
+		
+		#get revit_build
+		revit_branch = revit_info.splitlines()[1]
+		revit_branch = revit_branch.split(":",1)[1].strip()
+		
+		#return values
+		return revit_build, revit_branch
+		
+	except:
+		print("***Could not gather REVIT info.***")
 
 
 def extract_info_username(filename):
 	#pulls the username associated with the journal file into
 	#variable username
-	username = ""
-	with open(filename, 'r') as file_object:
-		lines = file_object.readlines()
-
-	for i, line in enumerate(lines):
-		if "jrn.directive" in line.lower() and "username" in line.lower(): 
-			for item in lines[max(i-0, 0):i+2]:
-				extracted_text = item.strip()
-	values = re.findall('"([^"]*)"', extracted_text)
-	username = values[0]
-	return username
+	import re
 	
+	username = ""
+	
+	try:
+		with open(filename, 'r') as file_object:
+			lines = file_object.readlines()
+
+		for i, line in enumerate(lines):
+			if "jrn.directive" in line.lower() and "username" in line.lower(): 
+				for item in lines[max(i-0, 0):i+2]:
+					extracted_text = item.strip()
+		values = re.findall('"([^"]*)"', extracted_text)
+		username = values[0]
+		return username
+		
+	except:
+		print("***Could not gather USERNAME info.***")
+
+def extract_info_ram(filename):
+	#parse a journal for ram information.
+	
+	#required modules
+	import math
+	
+	#Variables to store journal info to.
+	journal = ""
+	ram_record = []
+	
+	try:
+		#transform journal into parsable string
+		with open(filename, 'r') as file_object:
+			lines = file_object.readlines()
+			for line in lines:
+				journal = journal + line		
+		journal = journal.splitlines()
+		
+		#Extract the lines that reference ram in the journal.
+		for line in journal:
+			if "ram statistics" in line.lower():
+				data = line.split(':<')[1]
+				ram_record.append([int(s) for s in data.split() if s.isdigit()][0])
+				ram_max = [int(s) for s in data.split() if s.isdigit()][1]
+		
+		#Convert the extracted ram info into usable data.
+		ram_max = math.floor(int(ram_max) / 1000)
+		ram_avg = sum(ram_record) / len(ram_record) / 1000
+		ram_peak = sorted(ram_record)[-1] / 1000
+		
+		return ram_max, ram_avg, ram_peak
+	
+	except:
+		print("***Could not gather RAM info.***")
 	
 #execute
 for filename in cycle_journal_files('.'):
@@ -128,21 +186,42 @@ for filename in cycle_journal_files('.'):
 	print("-----------------------------------------------------------")
 	print(filename)
 	#username
-	print("User:", extract_info_username(filename))
+	try:
+		print("User:", extract_info_username(filename))
+	except:
+		print("USER FAILED")
 	#os info
-	print("OS Version:", extract_info_os(filename)[0])
-	print("OS Build:", extract_info_os(filename)[1])
+	try:
+		print("OS Version:", extract_info_os(filename)[0])
+		print("OS Build:", extract_info_os(filename)[1])
+	except:
+		print("OS FAILED")
 	#Revit info
-	print("Revit Build:", extract_info_revit(filename)[0])
-	print("Revit Branch:", extract_info_revit(filename)[1])
+	try:
+		print("Revit Build:", extract_info_revit(filename)[0])
+		print("Revit Branch:", extract_info_revit(filename)[1])
+	except:
+		print("REVIT FAILED")
 	#processor info
-	print("Processor Name:", extract_info_processor(filename)[0])
-	print("Processor Clockspeed:", extract_info_processor(filename)[1])
+	try:
+		print("Processor Name:", extract_info_processor(filename)[0])
+		print("Processor Clockspeed:", extract_info_processor(filename)[1])
+	except:
+		print("PROCESSOR FAILED")
 	#graphics info
-	print("GPU Name:", extract_info_graphics(filename)["graphics card:"])
-	print("GPU Manufacturer ID:", extract_info_graphics(filename)["manufacturer id:"])
-	print("GPU Device ID:", extract_info_graphics(filename)["device id:"])
-
+	try:
+		print("GPU Name:", extract_info_graphics(filename)["graphics card:"])
+		print("GPU Manufacturer ID:", extract_info_graphics(filename)["manufacturer id:"])
+		print("GPU Device ID:", extract_info_graphics(filename)["device id:"])
+	except:
+		print("GPU FAILED")
+	#ram info
+	try:
+		print("Max System RAM:", extract_info_ram(filename)[0], "GB")
+		print("Session Average RAM:", extract_info_ram(filename)[1], "GB")
+		print("Peak Session RAM:", extract_info_ram(filename)[2], "GB")
+	except:
+		print("RAM FAILED")
 
 
 
