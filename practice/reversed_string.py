0fa7532e35e97dce7ee619ed348c829a718dad65
Christian Gentry <christian.gentry@MBP-ChristianGentry.local>
2018-05-07 10:24:04 -0500

    reversed string challenge

---
+++ practice/reversed_string.py
@@ -0,0 +1,14 @@
+def FirstReverse(string): 
+	reverse = []
+	string = list(string)
+
+	while len(string) > 0:
+		reverse.append(string[-1])
+		string.pop()
+	
+	reverse = ''.join(reverse)
+
+	return reverse
+
+# keep this function call here  
+print(FirstReverse(input("Type out a sentence or word to see it reversed!: ")))
